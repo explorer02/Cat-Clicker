28de755a37dd05e03c6e9124dc0f58fe457c3cd3
Ajay <ajay.bhardwaj@sprinklr.com>
2021-07-19 15:14:01 +0530

    Cat Clicker v4

--- script.js
+++ script.js
@@ -1,32 +1,74 @@
-const cats = ["Cat 1", "Cat 2", "Cat 3", "Cat 4", "Cat 5"];
+(function () {
+  const model = {
+    selectedCat: null,
+    cats: [
+      { name: "Cat 1", count: 0, src: "./assets/cat1.jpeg" },
+      { name: "Cat 2", count: 0, src: "./assets/cat2.jpeg" },
+      { name: "Cat 3", count: 0, src: "./assets/cat3.jpeg" },
+      { name: "Cat 4", count: 0, src: "./assets/cat4.jpeg" },
+    ],
+    getAllCats() {
+      return this.cats;
+    },
+  };
 
-let counts = [];
-let currentCat = 0;
+  const octopus = {
+    init() {
+      model.selectedCat = model.cats[0];
+      viewCatList.init();
+      viewCanvas.init();
+    },
+    getAllCats() {
+      return model.getAllCats();
+    },
+    getSelectedCatData() {
+      return model.selectedCat;
+    },
+    updateCounter() {
+      model.selectedCat.count++;
+      viewCanvas.render();
+    },
+    updateSelectedCat(newCat) {
+      model.selectedCat = newCat;
+      viewCanvas.render();
+    },
+  };
+  const viewCatList = {
+    init() {
+      this.catList = document.querySelector(".catList");
+      this.render();
+    },
+    render() {
+      const allCats = octopus.getAllCats();
+      allCats.forEach((cat) => {
+        const p = document.createElement("p");
+        p.textContent = cat.name;
+        this.catList.append(p);
+        p.addEventListener("click", (ev) => {
+          octopus.updateSelectedCat(cat);
+        });
+      });
+    },
+  };
+  const viewCanvas = {
+    init() {
+      const canvas = document.querySelector(".canvas");
+      this.catImage = canvas.querySelector("img");
+      this.catTitle = canvas.querySelector(".title");
+      this.catCount = canvas.querySelector(".counter");
 
-function displayCatList() {
-  const catList = document.querySelector(".catList");
-  cats.forEach((e, i) => {
-    const p = document.createElement("p");
-    p.textContent = e;
-    p.addEventListener("click", updateCanvas.bind(null, i));
-    catList.append(p);
-    counts.push(0);
-  });
-}
-displayCatList();
+      viewCanvas.render();
+      this.catImage.addEventListener("click", (ev) => {
+        octopus.updateCounter();
+      });
+    },
+    render() {
+      const { name, count, src } = octopus.getSelectedCatData();
+      this.catImage.src = src;
+      this.catTitle.textContent = name;
+      this.catCount.textContent = `Count: ${count}`;
+    },
+  };
 
-function updateCanvas(i) {
-  currentCat = i;
-  const catImg = document.querySelector(".canvas>img");
-  const catTitle = document.querySelector(".canvas>.title");
-  const catCounter = document.querySelector(".canvas>.counter");
-  console.log(catImg);
-  catImg.src = `./assets/cat${i + 1}.jpeg`;
-
-  catTitle.textContent = cats[i];
-  catCounter.textContent = "Count:" + counts[i];
-}
-document.querySelector(".canvas>img").addEventListener("click", (ev) => {
-  counts[currentCat]++;
-  updateCanvas(currentCat);
-});
+  octopus.init();
+})();
