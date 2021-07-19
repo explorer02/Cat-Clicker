15f437850dad860893e02061ca85670bf0bf72b4
Ajay <ajay.bhardwaj@sprinklr.com>
2021-07-19 11:03:18 +0530

    Cat Clicker v2

--- script.js
+++ script.js
@@ -1,10 +1,39 @@
-const cat = document.querySelector("img");
-const counter = document.querySelector(".counter");
-let count = 0;
+const cats = ["Cat 1", "Cat 2", "Cat 3", "Cat 4"];
+const counters = document.querySelector(".counters");
+const container = document.querySelector(".container");
+let counts = [0];
 
-function updateCounter() {
-  counter.textContent = `Counter: ${count++}`;
+function displayCats() {
+  cats.forEach((e, i) => {
+    const div = document.createElement("div");
+    div.classList.add("cat-holder");
+    div.innerHTML = `
+        <p>${e}</p>
+        <img src="${`./assets/cat${i + 1}.jpeg`}"/>`;
+    container.appendChild(div);
+  });
 }
-updateCounter();
+displayCats();
 
-cat.addEventListener("click", updateCounter);
+function displayCounter() {
+  cats.forEach((e, i) => {
+    const div = document.createElement("div");
+    div.classList.add("counter" + (i + 1));
+    div.textContent = `Counter ${i + 1}: 0`;
+    counters.appendChild(div);
+    counts.push(0);
+  });
+}
+displayCounter();
+
+function updateCounter(i) {
+  document.querySelector(
+    `.counter${i}`
+  ).textContent = `Counter ${i}: ${++counts[i]}`;
+}
+
+document
+  .querySelectorAll("img")
+  .forEach((e, i) =>
+    e.addEventListener("click", updateCounter.bind(null, i + 1))
+  );
